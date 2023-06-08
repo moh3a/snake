050afdc5cc4bffd05c08fa90eaa00307959ff492
moh3a <aitabdelmalekma@gmail.com>
2023-06-08 21:37:36 +0100

    setup game tick and add web/js sys features

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,48 @@
 mod random;
 mod snake;
+
 use snake::SnakeGame;
 
+use js_sys::Function;
+use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::prelude::*;
+use web_sys::{console, window, HtmlElement};
+
+thread_local! {
+    static GAME: Rc<RefCell<SnakeGame>> = Rc::new(RefCell::new(SnakeGame::new(20, 20)));
+
+    static TICK_CLOSURE: Closure<dyn FnMut()> = Closure::wrap(Box::new({
+        let game = GAME.with(|game| game.clone());
+        move || game.borrow_mut().tick()
+    }) as Box<dyn FnMut()>);
+}
 
 #[wasm_bindgen(start)]
 pub fn main() {
-    let mut game = SnakeGame::new(20, 20);
+    console::log_1(&"Starting...".into());
 
-    todo!("setup the timer");
+    TICK_CLOSURE.with(|tick_closure| {
+        window()
+            .unwrap_throw()
+            .set_interval_with_callback_and_timeout_and_arguments_0(
+                tick_closure.as_ref().dyn_ref::<Function>().unwrap_throw(),
+                500,
+            )
+            .unwrap_throw()
+    });
+}
+
+pub fn render() {
+    let root_container = window()
+        .unwrap_throw()
+        .document()
+        .unwrap_throw()
+        .get_element_by_id("root")
+        .unwrap_throw()
+        .dyn_into::<HtmlElement>()
+        .unwrap_throw();
+
+    root_container.set_inner_html("");
+
+    todo!()
 }
